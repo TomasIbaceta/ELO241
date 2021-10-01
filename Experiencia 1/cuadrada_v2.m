f0fcc39d75c6a51e1ff1e45f18aadce726703fdd
TomasIbaceta <tomas.ibaceta@sansano.usm.cl>
2021-09-30 23:59:14 -0300

    cuadrada: se arreglan las a_0 y columna logb_0

--- Experiencia 1/cuadrada_v2.m
+++ Experiencia 1/cuadrada_v2.m
@@ -22,19 +22,33 @@ f=(a_0/2)+symsum(a_n*cos((2*n*pi*t)/T)+b_n*sin((2*n*pi*t)/T), n, 1, n_a);
 
 
 for i=DC
-    A = zeros(n_a,3);
+    A = zeros(n_a,4);
     a_n_ = subs(a_n,tau,i*T);
     b_n_ = subs(b_n,tau,i*T);
+    a_0_ = subs(a_0,tau,i*T);
+    
+    %agregar a mano los a_0 y b_0
+    A(1,1) = 0;
+    A(1,2) = subs(a_0_,n,0);
+    A(1,3) = 0;
+    
     for j = 1:n_a
-        A(j,1) = j;              %primera columna: n
-        A(j,2) = subs(a_n_,n,j); %segunda columna: a_n
-        A(j,3) = subs(b_n_,n,j); %tercera columna: b_n 
+        A(j+1,1) = j;              %primera columna: n
+        A(j+1,2) = subs(a_n_,n,j); %segunda columna: a_n
+        A(j+1,3) = subs(b_n_,n,j); %tercera columna: b_n 
         
         if vpa(10*log10(subs(a_n_,n,j))) < -100 %cuarta columna: 10*log(a_n)
-           A(j,4) = log(0); %no tiene sentido escribir -100 en la table
+           A(j+1,4) = log(0); %no tiene sentido escribir -100 en la table
         else
-            A(j,4) = 10*log10(subs(a_n_,n,j)); %cuarta columna: 10*log_10(a_n)
+            A(j+1,4) = 10*log10(subs(a_n_,n,j)); %cuarta columna: 10*log_10(a_n)
         end
+        
+        if vpa(10*log10(subs(b_n_,n,j))) < -100 %quinta columna: 10*log(b_n)
+           A(j+1,5) = log(0); %no tiene sentido escribir -100 en la table
+        else
+            A(j+1,5) = 10*log10(subs(b_n_,n,j)); %quinta columna: 10*log_10(b_n)
+        end
+        
     end
     mytitle = sprintf("DC:%d\\%%, n:%d",i*100,n_a);
     matrix2table(A,mytitle,1)
